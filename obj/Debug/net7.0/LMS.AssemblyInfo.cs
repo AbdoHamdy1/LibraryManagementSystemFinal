7e981500aa6f02f45ac7270203f7845b8b49e99d
AbdoHamdy1 <abdo7amdy123@gmail.com>
2024-10-18 21:07:38 +0300

    ERD,PowerPoint,Report

--- obj/Debug/net7.0/LMS.AssemblyInfo.cs
+++ obj/Debug/net7.0/LMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1756be44b808877b37b4875ff7a8bbdf685e7444")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76176d4e28031f166da430871abfd6365dc77e6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
